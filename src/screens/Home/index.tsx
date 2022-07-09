d2766c163855c4822b0f2f91fe5c240075d6a1a7
Handzic Haris <handzicharis62@yahoo.com>
2022-07-09 19:10:18 +0200

    template updates

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,54 @@
 import useTheme from 'hooks/useTheme'
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import {
+    PanGestureHandler,
+    PanGestureHandlerGestureEvent
+} from 'react-native-gesture-handler'
+import Animated, {
+    useSharedValue,
+    useAnimatedGestureHandler,
+    useAnimatedStyle
+} from 'react-native-reanimated'
 
 const Home = () => {
     const { theme } = useTheme()
+    const xVal = useSharedValue(0)
+    const yVal = useSharedValue(0)
 
+    const gestureHandler = useAnimatedGestureHandler<
+        PanGestureHandlerGestureEvent,
+        { x: number; y: number }
+    >({
+        onStart: (evt, ctx) => {
+            ctx.x = xVal.value
+            ctx.y = yVal.value
+        },
+        onActive: (evt, ctx) => {
+            xVal.value = evt.translationX + ctx.x
+            yVal.value = evt.translationY + ctx.y
+        }
+    })
+
+    const animatedStyle = useAnimatedStyle(() => ({
+        transform: [{ translateX: xVal.value }, { translateY: yVal.value }]
+    }))
     return (
         <View style={localStyles.container}>
-            <Text style={{ color: theme.secondary }}>Home</Text>
+            <PanGestureHandler onGestureEvent={gestureHandler}>
+                <Animated.View style={animatedStyle}>
+                    <Animated.Text style={[{ color: theme.secondary }]}>
+                        Home
+                    </Animated.Text>
+                </Animated.View>
+            </PanGestureHandler>
         </View>
     )
 }
 
 const localStyles = StyleSheet.create({
     container: {
-        backgroundColor: 'white',
+        backgroundColor: '#1A72A5',
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
